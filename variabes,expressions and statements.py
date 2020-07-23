98623563f72397e678981fa2fca9f0aef157a7cd
nikunj7878 <55085715+nikunj7878@users.noreply.github.com>
2020-07-24 00:20:56 +0530

    Add files via upload

---
+++ variabes,expressions and statements.py
@@ -0,0 +1,156 @@
+Python 3.8.3 (tags/v3.8.3:6f8c832, May 13 2020, 22:20:19) [MSC v.1925 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print(4)
+4
+>>> type('Hello, World!')
+<class 'str'>
+>>> type(17)
+<class 'int'>
+>>> type(3.2)
+<class 'float'>
+>>> type('17')
+<class 'str'>
+>>> type('3.2')
+<class 'str'>
+>>> print(1,000,000)
+1 0 0
+>>> message = 'And now for something completely different'
+>>> n = 17
+>>> pi = 3.1415926535897931
+>>> print(n)
+17
+>>> print(pi)
+3.141592653589793
+>>> type(message)
+<class 'str'>
+>>> type(n)
+<class 'int'>
+>>> typr(pi)
+Traceback (most recent call last):
+  File "<pyshell#14>", line 1, in <module>
+    typr(pi)
+NameError: name 'typr' is not defined
+>>> type(pi)
+<class 'float'>
+>>> 76trombones = 'big parade'
+SyntaxError: invalid syntax
+>>> 
+>>> print(1)
+1
+>>> print(x)
+Traceback (most recent call last):
+  File "<pyshell#19>", line 1, in <module>
+    print(x)
+NameError: name 'x' is not defined
+>>> x = 2
+>>> print (x)
+2
+>>> 20+32
+52
+>>> hour-1
+Traceback (most recent call last):
+  File "<pyshell#23>", line 1, in <module>
+    hour-1
+NameError: name 'hour' is not defined
+>>> hour-1
+Traceback (most recent call last):
+  File "<pyshell#24>", line 1, in <module>
+    hour-1
+NameError: name 'hour' is not defined
+>>> hour*60+minute
+Traceback (most recent call last):
+  File "<pyshell#25>", line 1, in <module>
+    hour*60+minute
+NameError: name 'hour' is not defined
+>>> mintue/60
+Traceback (most recent call last):
+  File "<pyshell#26>", line 1, in <module>
+    mintue/60
+NameError: name 'mintue' is not defined
+>>> 5**2
+25
+>>> (5+9)*(15-7)
+112
+>>>  minute = 59
+ 
+SyntaxError: unexpected indent
+>>> minute = 59
+>>> minute/60
+0.9833333333333333
+>>> minutr = 59
+>>> minute/60
+0.9833333333333333
+>>> 17
+17
+>>> x
+2
+>>> x+17
+19
+>>> 5
+5
+>>> x=5
+>>> x+1
+6
+>>> quotient = 7 // 3
+>>> print(quotient)
+2
+>>> remainder = 7 % 3
+>>> print(remainder)
+1
+>>> first = 10
+>>> second=15
+>>> print(first+second)
+25
+>>> first = '100'
+>>> second = '150'
+>>> print(first + second)
+100150
+>>> first = 'Test '
+>>> second = 3
+>>> print(first * second)
+Test Test Test 
+>>> inp = input()
+
+>>> print(inp)
+
+>>> name = input('What is your name?\n')
+What is your name?
+nikunj
+>>> print(name)
+nikunj
+>>> prompt = 'What...is the airspeed velocity of an unladen swallow?\n'
+>>> speed = input(prompt)
+What...is the airspeed velocity of an unladen swallow?
+17
+>>> int(speed)
+17
+>>> int(speed)+5
+22
+>>> compute the percentage of the hour that has elapsed
+SyntaxError: invalid syntax
+>>> percentage = (minute * 100) / 60
+>>> v = 5
+>>> a = 35.0
+>>> b = 12.5
+>>> c = a*b
+>>> print(c)
+437.5
+>>> hours = 35.0
+>>> rate = 12.50
+>>> pay = hours*rate
+>>> print(pay)
+437.5
+>>> x1q3z9ahd = 35.0
+>>> x1q3z9afd = 12.50
+>>> x1q3p9afd = x1q3z9ahd * x1q3z9afd
+>>> print(x1q3p9afd)
+437.5
+>>> bad name = 5
+SyntaxError: invalid syntax
+>>>  month = 09
+ 
+SyntaxError: unexpected indent
+>>> month = 09
+SyntaxError: leading zeros in decimal integer literals are not permitted; use an 0o prefix for octal integers
+>>> month = 12
+>>> 
